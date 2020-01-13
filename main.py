bf70ef7d6996063dbe537ee8ab348973092e4bd9
Noob-crypto <fatimacivil1993@gmail.com>
2020-01-13 16:29:26 +0000

    final

--- main.py
+++ main.py
@@ -9,24 +9,42 @@ soup = BeautifulSoup(c,'html.parser')
 all = soup.find_all('div',{'class':'propertyRow'})
 
 all[0].find('h4',{'class':'propPrice'}).text.replace("\n","").replace(' ','')
+
+
+l=[]
 for item in all:
-    print(item.find('h4',{'class':'propPrice'}).text.replace("\n","").replace(' ',''))
-    print(item.find_all('span',{'class':'propAddressCollapse'})[0].text)
-    print(item.find_all('span',{'class':'propAddressCollapse'})[1].text)
+    d={}
+    d["Price"]=item.find('h4',{'class':'propPrice'}).text.replace("\n","").replace(' ','')
+    d["Address"]=item.find_all('span',{'class':'propAddressCollapse'})[0].text
+    d["Locality"]=item.find_all('span',{'class':'propAddressCollapse'})[1].text
     try:
-        print(item.find('span',{'class':'infoBed'}).find('b').text)
+        d["Beds"]=item.find('span',{'class':'infoBed'}).find('b').text
     except:
-        print(None)
+        d["Beds"]=None
     try:
-        print(item.find('span',{'class':'infoSqFt'}).find('b').text)
+        d["Area"]=item.find('span',{'class':'infoSqFt'}).find('b').text
     except:
-        print(None)
+        d["Area"]=None
     try:
-        print(item.find('span',{'class':'infoValueFullBath'}).find('b').text)
+        d["Full Bath"]=item.find('span',{'class':'infoValueFullBath'}).find('b').text
     except:
-        print(None)
+        d["Full Bath"]=None
     try:
-        print(item.find('span',{'class':'infoValueHalfBath'}).find('b').text)
+        d["Half Bath"]=item.find('span',{'class':'infoValueHalfBath'}).find('b').text
     except:
-        print(None)    
-    print('')
+        d["Half Bath"]=None
+        
+    for column_group in item.find_all("div",{"class":"columnGroup"}):
+        for feature_group, feature_name in zip (column_group.find_all('span',{'class':'featureGroup'}),column_group.find_all('span',{'featureName'})):
+            if 'Lot Size' in feature_group.text:
+                d["Lot Size"]=feature_name.text
+    
+    
+    l.append(d)
+
+
+
+import pandas 
+df=pandas.DataFrame(l)
+
+print(df)
