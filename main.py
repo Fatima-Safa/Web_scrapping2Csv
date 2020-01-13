6bae9936dc55222b1e4e01b8112439024abd357d
Noob-crypto <fatimacivil1993@gmail.com>
2020-01-13 09:35:54 +0000

    beds_error

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("http://www.pyclass.com/real-estate/rock-springs-wy/LCWYROCKSPRINGS/", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+c = r.content
+
+soup = BeautifulSoup(c,'html.parser')
+
+all = soup.find_all('div',{'class':'propertyRow'})
+
+all[0].find('h4',{'class':'propPrice'}).text.replace("\n","").replace(' ','')
+for item in all:
+    print(item.find('h4',{'class':'propPrice'}).text.replace("\n","").replace(' ',''))
+    print(item.find_all('span',{'class':'propAddressCollapse'})[0].text)
+    print(item.find_all('span',{'class':'propAddressCollapse'})[1].text)
+    try:
+        print((item.find('span',{'class':'infoBed'}).text)      
+    except:
+        print(None)  
+    
+    print('')
